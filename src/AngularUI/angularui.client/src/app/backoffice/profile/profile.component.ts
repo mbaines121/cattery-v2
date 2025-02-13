df8a00202b0334439e551fec2a21b440933bf51d
Mark Baines <markbaines121@hotmail.com>
2025-02-13 12:25:51 +0000

    Added a basic profile page from auth0 docs.

--- src/AngularUI/angularui.client/src/app/backoffice/profile/profile.component.ts
+++ src/AngularUI/angularui.client/src/app/backoffice/profile/profile.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { AsyncPipe, CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [
+    AsyncPipe, 
+    CommonModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  auth = inject(AuthService);
+  profileJson: string | null = null;
 
+  ngOnInit(): void {
+    this.auth.user$.subscribe(
+      (profile) => (this.profileJson = JSON.stringify(profile, null, 2)),
+    );
+  }
 }
